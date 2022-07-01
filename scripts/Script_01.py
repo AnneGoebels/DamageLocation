ee722225de7cc26fac6039294ec119f9947646b2
Anne Göbels <annegoebels95@gmail.com>
2022-07-01 10:04:06 +0200

    changed script structure, add lbd to bauteil instead of bdef

--- scripts/Script_01.py
+++ scripts/Script_01.py
@@ -2,98 +2,123 @@ from func import Control, q_main, Bauteil
 import ifcopenshell
 from ifcopenshell.util.selector import Selector
 import json
+from rdflib import Namespace
 
-def createControlIfc (sibbw_data_file,ifc_file,lbd_of_ifc_file,bt_file) :
+def createControlIfc (sibbw_graph, ifc_file, ifc_lbd_graph, bt_file) :
+
+    selector =  Selector()
+    x =selector.parse(ifc_file, ".IfcRailing")
+    print(x)
+
     settings = ifcopenshell.geom.settings()
     settings.set(settings.USE_PYTHON_OPENCASCADE, True)
 
+    #opens turtle files
+    ANS = Namespace("https://w3id.org/asbingowl/core#")
     ### Building component location begins here:
 
-    Control.create_bt_file(ifc_file, bt_file)
+    #model, f = Control.create_bt_file(ifc_file, bt_file)
    # model = ifcopenshell.open(ifc_file)
    # f = ifcopenshell.open(bt_file)
 
-    bauteildefinition_list = q_main.get_bauteildefinition(sibbw_data_file)
+    bauteildefinition_list = q_main.get_bauteildefinition(sibbw_graph)
+    #print(bauteildefinition_list)
 
     with open(r".\temp_files\map_bauteilTyp.json", "r") as file:
         map_bT = json.loads(file.read())
 
     unlocatedBdef = []
+    locatedBauteile = []
 
     for bauteildefinition in bauteildefinition_list:
 
-        print("_____________________________________________________")
-        print(bauteildefinition)
+        asbBauteilClassList, asbBauteilInstanceList = q_main.get_ifcBridgeName(sibbw_graph, bauteildefinition)
+        print(asbBauteilClassList, asbBauteilInstanceList)
 
-        dictionary_oa = q_main.get_Ortsangabe(sibbw_data_file, bauteildefinition)[0]
-        dictionary_tesBauteil = q_main.get_Ortsangabe(sibbw_data_file, bauteildefinition)[1]
-        dictionary_feld = q_main.get_Ortsangabe(sibbw_data_file, bauteildefinition)[2]
-        dictionary_links_rechts = q_main.get_Ortsangabe(sibbw_data_file, bauteildefinition)[3]
-        dictionary_anfang_ende = q_main.get_Ortsangabe(sibbw_data_file, bauteildefinition)[4]
+        for asbBauteilInstance in asbBauteilInstanceList:
 
-        ifcBridgeName_list, asbBauteilList = q_main.get_ifcBridgeName(sibbw_data_file, bauteildefinition)
-
-        print(asbBauteilList)
-
-        if len(ifcBridgeName_list) > 0:
-            bT = []
-
-            for j in ifcBridgeName_list:
-                res = map_bT[j]
-                if "ignore" not in res and res not in bT:
-                    bT.append(res)
-
-            if len(bT) > 0:
-                ifcBridgeName = bT[0]
+            if asbBauteilInstance in locatedBauteile:
+                print("already located")
             else:
-                ifcBridgeName = None
-                print("No ifcBridgeName found")
-        else:
-            ifcBridgeName = None
-            print("BauteilDef not connected to ASB-Bauteil")
+                print(bauteildefinition)
+                bdef_props = q_main.query_bauteildefinition(sibbw_graph, bauteildefinition)
+                #print(bdef_props)
+                dictionaries = q_main.get_Ortsangabe(bdef_props)
 
-        print("Nicht klassifizierte Ortsangaben: ", end='')
-        print(dictionary_oa)
-        print("Ortsangaben Anfang/Ende: ", end='')
-        print(dictionary_anfang_ende)
-        print("Ortsangaben für Bauteilsuche: ", end='')
-        print(dictionary_tesBauteil)
-        print("Feld :", end='')
-        print(dictionary_feld)
-        print("Links/Rechts:", end='')
-        print(dictionary_links_rechts)
-        print("ifcBridgeName: ")
-        print(ifcBridgeName)
-
-        if ifcBridgeName is not None:
-
-            if len(dictionary_links_rechts) > 0:
-                for counter, i in enumerate(dictionary_links_rechts.values()):
-                    print(counter,i)
-
-                    addIfctoRDF = Bauteil.check_model_representation(asbBauteilList,bauteildefinition, sibbw_data_file, ifc_file, bt_file,
-                                                                  lbd_of_ifc_file, ifcBridgeName, i, dictionary_tesBauteil,
-                                                                  dictionary_feld, dictionary_anfang_ende)
-
-                    print(addIfctoRDF)
-                    if addIfctoRDF is None:
-                        unlocatedBdef.append(bauteildefinition)
+                #print(dictionaries)
+                dictionary_oa = dictionaries[0]
+                dictionary_tesBauteil = dictionaries[1]
+                dictionary_feld = dictionaries[2]
+                dictionary_links_rechts = dictionaries[3]
+                dictionary_anfang_ende =dictionaries[4]
 
 
-            else:
-                addIfctoRDF = Bauteil.check_model_representation(asbBauteilList, bauteildefinition, sibbw_data_file, ifc_file, bt_file,
-                                                              lbd_of_ifc_file, ifcBridgeName, dictionary_links_rechts,
-                                                              dictionary_tesBauteil, dictionary_feld,
-                                                              dictionary_anfang_ende)
+                if len(asbBauteilClassList) > 0:
+                    bT = []
 
-                print(addIfctoRDF)
-                if addIfctoRDF is None:
+                    for j in asbBauteilClassList:
+                        print(str(j))
+                        try:
+                            res = map_bT[str(j)]
+                            if "ignore" not in res and res not in bT:
+                                bT.append(res)
+                        except:
+                            print(j)
+
+                    if len(bT) > 0:
+                        ifcBridgeName = bT[0]
+                    else:
+                        ifcBridgeName = None
+                        print("No ifcBridgeName found")
+                else:
+                    ifcBridgeName = None
+                    print("BauteilDef not connected to ASB-Bauteil")
+
+                print("Nicht klassifizierte Ortsangaben: ", end='')
+                print(dictionary_oa)
+                print("Ortsangaben Anfang/Ende: ", end='')
+                print(dictionary_anfang_ende)
+                print("Ortsangaben für Bauteilsuche: ", end='')
+                print(dictionary_tesBauteil)
+                print("Feld :", end='')
+                print(dictionary_feld)
+                print("Links/Rechts:", end='')
+                print(dictionary_links_rechts)
+                print("ifcBridgeName: ")
+                print(ifcBridgeName)
+
+                if ifcBridgeName is not None:
+
+                    if len(dictionary_links_rechts) > 0:
+                        for counter, i in enumerate(dictionary_links_rechts.values()):
+                            print(counter,i)
+
+                            ifc_lbd_instance = Bauteil.check_model_representation( bauteildefinition, bdef_props, ifc_file, bt_file,
+                                                                             ifc_lbd_graph, ifcBridgeName, i, dictionary_tesBauteil,
+                                                                             dictionary_feld, dictionary_anfang_ende)
+
+                            print(ifc_lbd_instance)
+                            if ifc_lbd_instance is not None:
+                                sibbw_graph.add((asbBauteilInstance, ANS.hasModelRepresentation, ifc_lbd_instance))
+                                locatedBauteile.append(asbBauteilInstance)
+                            else:
+                                unlocatedBdef.append(asbBauteilInstance)
+
+
+                    else:
+                        ifc_lbd_instance = Bauteil.check_model_representation( bauteildefinition, bdef_props, ifc_file, bt_file,
+                                                                         ifc_lbd_graph, ifcBridgeName, dictionary_links_rechts,
+                                                                         dictionary_tesBauteil, dictionary_feld,
+                                                                         dictionary_anfang_ende)
+                        print(ifc_lbd_instance)
+                        if ifc_lbd_instance is not None:
+                            sibbw_graph.add((asbBauteilInstance, ANS.hasModelRepresentation, ifc_lbd_instance))
+                            locatedBauteile.append(asbBauteilInstance)
+                        else:
+                            unlocatedBdef.append(asbBauteilInstance)
+
+                else:
                     unlocatedBdef.append(bauteildefinition)
-
-
-
-        else:
-            unlocatedBdef.append(bauteildefinition)
 
     print(unlocatedBdef)
 
